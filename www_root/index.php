29079e6bc6898f999bf51e21c0f1c887285f8308
sedli94 <74666105+sedli94@users.noreply.github.com>
2021-01-17 23:42:26 +0100

    Update index.php

--- www_root/index.php
+++ www_root/index.php
@@ -1,10 +1,6 @@
 <?php 
 declare(strict_types=1);
 
- if ('185.112.167.176' != $_SERVER['REMOTE_ADDR'] && '46.135.68.141' != $_SERVER['REMOTE_ADDR'] ) {
-	die;
-}
-
 require __DIR__ . '/../vendor/autoload.php';
 
 App\Bootstrap::boot()
